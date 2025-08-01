b6c0d80f7992b3476c58361fc5a771ba10dde065
Sakai12404 <nathansanchez745@gmail.com>
2025-08-01 10:25:42 -0400

    I split of main.dart to make everything more readlable I hope

---
+++ lib/screen/checklist.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/models/task.dart';
+import 'package:to_do_list/services/task_storage.dart';
+
+class CheckList extends StatefulWidget {
+  const CheckList({super.key});
+  @override
+  State<CheckList> createState() => _CheckListState();
+}
+
+class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
+  List<Task> tasks = [];
+  final TextEditingController _controller = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    loadTasks();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.inactive ||
+        state == AppLifecycleState.detached) {
+      saveTasks();
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do List", style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return _buildTaskBoxCheckListCard(task,index);
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniStartDocked,
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () {
+          setState(() {
+            _controller.clear();
+            _showTaskCreationScreen(context, _controller);
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void saveTasks() async {
+    await saveToFile(tasks);
+  }
+
+  void loadTasks() async {
+    final loaded = await readTasks();
+    setState(() {
+      tasks = loaded;
+    });
+  }
+
+  Card _buildTaskBoxCheckListCard(Task task,int index) {
+    return Card(
+      color: Colors.black,
+      child: Dismissible(
+        background: Card(color: Colors.red, child: Icon(Icons.remove_circle)),
+        key: UniqueKey(),
+        onDismissed: (DismissDirection direction) => setState(() {
+          tasks.removeAt(index);
+        }),
+        confirmDismiss: (DismissDirection direction) async {
+          return await showDialog(
+            context: context,
+            builder: (context) => _buildConfirmationAlertDialog(task),
+          );
+        },
+        child: _buildTaskBoxCheckList(task),
+      ),
+    );
+  }
+
+  Widget _buildConfirmationAlertDialog(Task task) {
+    return AlertDialog(
+      title: Text("Delete Task"),
+      content: Text("Are you sure you want to delete ${task.task}?"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: Text("confirm"),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTaskBoxCheckList(Task task) {
+    return CheckboxListTile(
+      title: Text(
+        'Task: ${task.task}',
+        style: TextStyle(
+          color: Colors.white,
+          decoration: task.isDone ? TextDecoration.lineThrough : null,
+          decorationColor: Color.fromARGB(255, 234, 221, 255),
+          decorationThickness: 3.5,
+        ),
+      ),
+      subtitle: Text(
+        "Due: ${task.dateTime.toLocal().toString().split(' ')[0]}",
+        style: TextStyle(
+          color: Colors.white,
+          decoration: task.isDone ? TextDecoration.lineThrough : null,
+          decorationColor: Color.fromARGB(255, 234, 221, 255),
+          decorationThickness: 3.5,
+        ),
+      ),
+      value: task.isDone,
+      controlAffinity: ListTileControlAffinity.leading,
+      activeColor: Colors.white,
+      checkColor: Color.fromARGB(255, 79, 57, 140),
+      onChanged: (bool? value) {
+        setState(() {
+          task.isDone = value ?? false;
+        });
+      },
+    );
+  }
+
+  // ignore: no_leading_underscores_for_local_identifiers
+  void _showTaskCreationScreen(BuildContext context,TextEditingController _controller) {
+    DateTime? selectedDate = DateTime.now();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Your task"),
+        content: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Task',
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextButton.icon(
+              icon: Icon(Icons.calendar_today_rounded),
+              label: Text(
+                "Due: ${selectedDate!.toLocal().toString().split(' ')[0]}",
+              ),
+              onPressed: () async {
+                selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2100),
+                );
+                setState(() => selectedDate ??= DateTime.now());
+              },
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            child: Text("submit"),
+            onPressed: () {
+              final input = _controller.text.trim();
+              if (input.isNotEmpty) {
+                setState(() {
+                  tasks.add(Task(input, false, selectedDate!));
+                  tasks.sort((a, b) => a.dateTime.compareTo(b.dateTime));
+                });
+              }
+              Navigator.pop(context);
+            },
+          ),
+          TextButton(
+            child: Text("cancel"),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
