250ce2c329b8a39125f2d402bd4163042aa62d1b
Sakai12404 <nathansanchez745@gmail.com>
2025-07-31 21:54:15 -0400

    Made all the code more readable

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    loadedTasks();
+    loadTasks();
   }
 
   @override
@@ -50,7 +50,7 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
     super.didChangeAppLifecycleState(state);
     if (state == AppLifecycleState.inactive ||
         state == AppLifecycleState.detached) {
-      saveTask();
+      saveTasks();
       return;
     }
   }
@@ -69,75 +69,7 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
               itemCount: tasks.length,
               itemBuilder: (context, index) {
                 final task = tasks[index];
-                return Card(
-                  color: Colors.black,
-                  child: Dismissible(
-                    background: Card(
-                      color: Colors.red,
-                      child: Icon(Icons.remove_circle),
-                    ),
-                    key: UniqueKey(),
-                    onDismissed: (DismissDirection direction) {
-                      setState(() {
-                        tasks.removeAt(index);
-                      });
-                    },
-                    confirmDismiss: (DismissDirection direction) async {
-                      return await showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: Text('Delete Task'),
-                          content: Text(
-                            "Are you sure you want to delete ${task.task}?",
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: Text("Confirm"),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(false),
-                              child: Text("Cancel"),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    child: CheckboxListTile(
-                      title: Text(
-                        'Task: ${task.task}',
-                        style: TextStyle(
-                          color: Colors.white,
-                          decoration: task.isDone
-                              ? TextDecoration.lineThrough
-                              : null,
-                          decorationColor: Color.fromARGB(255, 234, 221, 255),
-                          decorationThickness: 3.5,
-                        ),
-                      ),
-                      subtitle: Text(
-                        "Due: ${task.dateTime.toLocal().toString().split(' ')[0]}",
-                        style: TextStyle(
-                          color: Colors.white,
-                          decoration: task.isDone
-                              ? TextDecoration.lineThrough
-                              : null,
-                          decorationColor: Color.fromARGB(255, 234, 221, 255),
-                          decorationThickness: 3.5,
-                        ),
-                      ),
-                      value: task.isDone,
-                      controlAffinity: ListTileControlAffinity.leading,
-                      activeColor: Colors.white,
-                      checkColor: Color.fromARGB(255, 79, 57, 140),
-                      onChanged: (bool? value) {
-                        setState(() {
-                          task.isDone = value ?? false;
-                        });
-                      },
-                    ),
-                  ),
-                );
+                return _buildTaskBoxCheckListCard(task,index); /**/
               },
             ),
           ),
@@ -145,8 +77,6 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
       ),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
-
-        //child: Container(height: 25),
       ),
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniStartDocked,
@@ -154,7 +84,7 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
         onPressed: () {
           setState(() {
             _controller.clear();
-            _showDialog(context, _controller);
+            _showTaskCreationScreen(context, _controller);
           });
         },
         child: const Icon(Icons.add),
@@ -162,22 +92,91 @@ class _CheckListState extends State<CheckList> with WidgetsBindingObserver {
     );
   }
 
-  void saveTask() async {
+  void saveTasks() async {
     await saveToFile(tasks);
   }
 
-  void loadedTasks() async {
+  void loadTasks() async {
     final loaded = await readTasks();
     setState(() {
       tasks = loaded;
     });
   }
 
+  Card _buildTaskBoxCheckListCard(Task task,int index) {
+    return Card(
+      color: Colors.black,
+      child: Dismissible(
+        background: Card(color: Colors.red, child: Icon(Icons.remove_circle)),
+        key: UniqueKey(),
+        onDismissed: (DismissDirection direction) => setState(() {
+          tasks.removeAt(index);
+        }),
+        confirmDismiss: (DismissDirection direction) async {
+          return await showDialog(
+            context: context,
+            builder: (context) => _buildConfirmationAlertDialog(task),
+          );
+        },
+        child: _buildTaskBoxCheckList(task),
+      ),
+    );
+  }
+
+  Widget _buildConfirmationAlertDialog(Task task) {
+    return AlertDialog(
+      title: Text("Delete Task"),
+      content: Text("Are you sure you want to delete ${task.task}?"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: Text("confirm"),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTaskBoxCheckList(Task task) {
+    return CheckboxListTile(
+      title: Text(
+        'Task: ${task.task}',
+        style: TextStyle(
+          color: Colors.white,
+          decoration: task.isDone ? TextDecoration.lineThrough : null,
+          decorationColor: Color.fromARGB(255, 234, 221, 255),
+          decorationThickness: 3.5,
+        ),
+      ),
+      subtitle: Text(
+        "Due: ${task.dateTime.toLocal().toString().split(' ')[0]}",
+        style: TextStyle(
+          color: Colors.white,
+          decoration: task.isDone ? TextDecoration.lineThrough : null,
+          decorationColor: Color.fromARGB(255, 234, 221, 255),
+          decorationThickness: 3.5,
+        ),
+      ),
+      value: task.isDone,
+      controlAffinity: ListTileControlAffinity.leading,
+      activeColor: Colors.white,
+      checkColor: Color.fromARGB(255, 79, 57, 140),
+      onChanged: (bool? value) {
+        setState(() {
+          task.isDone = value ?? false;
+        });
+      },
+    );
+  }
+
   // ignore: no_leading_underscores_for_local_identifiers
-  void _showDialog(BuildContext context, TextEditingController _controller) {
+  void _showTaskCreationScreen(BuildContext context,TextEditingController _controller) {
     DateTime? selectedDate = DateTime.now();
     showDialog(
-      //barrierColor: Colors.black,
+      //I think i can make this more readable
       context: context,
       builder: (context) => AlertDialog(
         title: Text("Your task"),
@@ -249,9 +248,12 @@ Future<List<Task>> readTasks() async {
 
     final contents = await file.readAsString();
     final List<dynamic> decoded = jsonDecode(contents);
-    return decoded
-        .map((item) => Task(item[0], item[1], DateTime.parse(item[2])))
-        .toList();
+    List<Task> loadedData = [];
+    for (var i in decoded) {
+      i as Map<String, dynamic>;
+      loadedData.add(Task(i["task"], i["complete"], DateTime.parse(i["due"])));
+    }
+    return loadedData;
   } catch (e) {
     return [];
   }
@@ -259,10 +261,13 @@ Future<List<Task>> readTasks() async {
 
 Future<void> saveToFile(List<Task> tasks) async {
   final File file = await getLocalFile();
-  List<List<dynamic>> toWrite = [];
+  List<Map<String, dynamic>> toWrite = [];
   for (var i in tasks) {
-    toWrite.add([i.task, i.isDone, i.dateTime.toIso8601String()]);
+    toWrite.add({
+      "task": i.task,
+      "complete": i.isDone,
+      "due": i.dateTime.toIso8601String(),
+    });
   }
   await file.writeAsString(jsonEncode(toWrite));
 }
-//finsih card
